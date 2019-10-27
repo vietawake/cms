aa8911599f5a24ed5c2280318e544b8636193e19
vietawake <vietawake@gmail.com>
2019-10-27 13:09:22 +0700

    init 3

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,6 +2,6 @@
 IP=$1
 
 scp -i /opt/prod ./jenkins/deploy/publish root@$IP:/tmp/publish
-scp -i /opt/prod -prq sources/ root@$IP:~/SUM_CMS
+scp -i /opt/prod -prq sources root@$IP:~/SUM_CMS
 scp -i /opt/prod -prq docker-compose.yml root@$IP:~/SUM_CMS
 ssh -i /opt/prod root@$IP "sudo bash /tmp/publish"
